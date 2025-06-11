69a13cc81b1e7c826febcbca8912c7dcb3448267
Alberto Mercurio <alberto.mercurio96@gmail.com>
2025-06-12 01:21:41 +0200

    Minimal Working Example

--- src/components/BohrAtom.vue
+++ src/components/BohrAtom.vue
@@ -1,14 +1,11 @@
 <template>
-    <svg ref="rootRef" :width="size" :height="size" :viewBox="viewBox">
-        <!-- Nucleus -->
-         <circle :cx="center" :cy="center" :r="nucleonRadius" class="nucleon proton" />
+    <svg ref="rootRef" width=200 height=200 viewBox="0 0 200 200">
+        <circle cx=100 cy=100 r=5 class="nucleon proton" />
 
-        <!-- Orbitals -->
-        <ellipse :cx="center" :cy="center" :rx="radiusStep" :ry="radiusStep" :transform="`rotate(45 ${center} ${center})`" class="orbit" />
-        <ellipse :cx="center" :cy="center" :rx="2*1.5*radiusStep" :ry="2*radiusStep" :transform="`rotate(45 ${center} ${center})`" class="orbit" />
+        <ellipse cx=100 cy=100 rx=20 ry=20 class="orbit orbit1" />
+        <ellipse cx=100 cy=100 rx=60 ry=40 class="orbit orbit2" />
 
-        <!-- Electron -->
-        <circle :cx="center" :cy="center" r="4" class="electron" />
+        <circle cx=100 cy=100 r=4 class="electron" />
     </svg>
 </template>
 
@@ -19,86 +16,46 @@ import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
 
 gsap.registerPlugin(MotionPathPlugin)
 
-const props = defineProps({
-    size: { type: Number, default: 400 },
-})
-
-const center = props.size / 2
-const radiusStep = props.size / 8
-const viewBox = `0 0 ${props.size} ${props.size}`
-const nucleonRadius = 4
-
 const rootRef = ref(null);
-const orbitsList = ref([]);
-const electronsList = ref([[]]);
 const ctx = ref(null);
-
-defineExpose({
-    rootRef,
-    orbitsList,
-    electronsList,
-})
 
 onMounted(() => {
     console.log('BohrAtom mounted');
-    populateOrbitsList(rootRef.value);
-    populateElectronsList(rootRef.value, orbitsList.value);
-    startAnimation(orbitsList.value, electronsList.value);
+
+    ctx.value = gsap.context(() => {
+        const orbit1 = document.querySelector('.orbit1')
+        const orbit2 = document.querySelector('.orbit2')
+        const electron = document.querySelector('.electron')
+
+        gsap.set(orbit2, {
+            rotation: 45,
+            transformOrigin: 'center center'
+        })
+
+        gsap.to(electron, {
+            attr: {
+                cx: getOrbitRelativeDistance(electron, orbit2).x,
+                cy: getOrbitRelativeDistance(electron, orbit2).y
+            },
+            duration: 2,
+        })
+    }, rootRef.value);
 })
 
 onUnmounted(() => {
     ctx.value?.revert(); // Cleanup GSAP context
 })
 
-function startAnimation(orbitsList, electronsList) {
-    ctx.value = gsap.context(() => {
-        const [orbitPath] = MotionPathPlugin.convertToPath(orbitsList[0]);
-        const timeline = gsap.timeline();
-        timeline.to(electronsList[0][0], {
-            motionPath: {
-                path: orbitPath,
-                align: orbitPath,
-                alignOrigin: [0.5, 0.5],
-                start: 0,
-                end: 1,
-            },
-            duration: 2,
-            repeat: 3,
-            ease: "linear",
-        });
 
-        timeline.to(electronsList[0][0], {
-            x: getOrbitCoordinates(orbitsList[1]).x,
-            y: getOrbitCoordinates(orbitsList[1]).y,
-            duration: 1,
-            ease: "power1.inOut",
-        });
-    });
-}
+function getOrbitRelativeDistance(electron, targetOrbit) {
+    const targetOrbitPath = MotionPathPlugin.convertToPath(targetOrbit)[0];
+    const orbitPathRaw = MotionPathPlugin.getRawPath(targetOrbitPath);
+    MotionPathPlugin.cacheRawPathMeasurements(orbitPathRaw);
+    const p = MotionPathPlugin.getPositionOnPath(orbitPathRaw , 0.3);
 
-function getOrbitCoordinates(targetOrbit) {
-  const targetOrbitPath = MotionPathPlugin.convertToPath(targetOrbit)[0];
-  const orbitPathArray = MotionPathPlugin.getRawPath(targetOrbitPath);
-  console.log('Target Orbit:', targetOrbitPath);
-  console.log('Orbit Path Array:', orbitPathArray);
-  MotionPathPlugin.cacheRawPathMeasurements(orbitPathArray);
-  const pathPoint = MotionPathPlugin.getPositionOnPath(orbitPathArray , 0.0);
-  console.log('Point on Target Orbit:', pathPoint);
+    console.log('Position on path:', p);
 
-  return pathPoint;
-}
-
-// Populate the orbits and electrons lists based on the SVG structure at runtime
-// This avoids possible conflicts
-function populateOrbitsList(rootRef) {
-    orbitsList.value = Array.from(rootRef.querySelectorAll('.orbit'));
-}
-
-function populateElectronsList(rootRef, orbitsList) {
-    orbitsList.forEach((_, orbitIndex) => {
-        const electrons = rootRef.querySelectorAll(`.electron`);
-        electronsList.value[orbitIndex] = Array.from(electrons);
-    });
+    return p;
 }
 </script>
 
